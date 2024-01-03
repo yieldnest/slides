3de1a4f54d7a689908cd1c0fe9f894b242c812c2
Shawn Fladager <shawnfladager@gmail.com>
2024-01-03 15:53:40 +0700

    Add link icon.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { ExternalLink } from 'lucide-react'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background">
       <Image src="/yieldnest-dark.svg" alt="YieldNest Logo" width={300} height={300} />
-      <Link className="py-4" href="/yieldnest-slides.pdf" locale={false} aria-label="YieldNest Pitchdeck" > 
-        <button className="p-8 text-xl sm:text-3xl font-bold text-foreground hover:text-secondary">{`View YieldNest's Pitchdeck`}</button> 
+      <Link className="py-8 flex justify-center items-center gap-2" href="/yieldnest-slides.pdf" locale={false} aria-label="YieldNest Pitchdeck" > 
+        <button className="text-xl sm:text-3xl font-bold text-foreground hover:text-secondary">{`View YieldNest's Pitchdeck`}</button> 
+        <ExternalLink className='align-middle h-6 w-6 text-secondary'/>
       </Link>
     </main>
   )
