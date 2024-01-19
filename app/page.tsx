22101fc8dac0f1cb4ed9c241db0e89b4ccc1e6b5
Shawn Fladager <shawnfladager@gmail.com>
2024-01-19 11:03:10 +0700

    Updating slides v2.1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background">
       <Image src="/yieldnest-dark.svg" alt="YieldNest Logo" width={300} height={300} />
-      <Link className="py-8 flex justify-center items-center gap-2" href="/yieldnest-slides-v2.pdf" locale={false} aria-label="YieldNest Pitchdeck" > 
+      <Link className="py-8 flex justify-center items-center gap-2" href="/yieldnest-slides-v2.1.pdf" locale={false} aria-label="YieldNest Pitchdeck" > 
         <button className="text-xl sm:text-3xl font-bold text-foreground hover:text-secondary">{`View YieldNest's Pitchdeck`}</button> 
         <ExternalLink className='align-middle h-6 w-6 text-secondary'/>
       </Link>
